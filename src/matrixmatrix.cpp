afc437b30d6f5fe00909004856a9ab3e449e5347
jaschn <49870201+jaschn@users.noreply.github.com>
2019-06-29 03:14:39 +0200

    terroristisch fertig

--- src/matrixmatrix.cpp
+++ src/matrixmatrix.cpp
@@ -75,20 +75,50 @@ void main_task(int rank, int world_size)
 		space[i] = space[i-1] + partitions[i-1];
 	}
 	std::vector<double> tmp(part_size_l);
-	std::cout << "start" << std::endl;
 	for(int i = 0; i<m;i++)
 	{
-		MPI_Scatterv(&(matA[i][0]),&(partitions[0]),&space[0],MPI_DOUBLE, NULL,0, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		MPI_Scatterv(&(matA[i][0]),&partitions[0],&space[0],MPI_DOUBLE, NULL,0, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 	}
 	for(int i = 1; i< world_size;i++)
 	{
-		std::cout << "test: " << i << std::endl;
 		int start = i*part_size_n;
 		int end = i!=world_size-1 ? (i+1) * part_size_n : i*part_size_n + part_size_n_last;
+		MPI_Send(&start, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&end, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 		for(int j = start;j<end;j++)
 		{
 			MPI_Send(&matB[j][0], m, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
 		}
+	}
+	int b_start = 0;
+	int b_end = part_size_n;
+	for(int i = world_size-1;i>=0;i--)
+	{
+		if(i!=world_size-1)
+		{
+			b_start = i*part_size_n;
+			b_end = i!=world_size-1 ? (i+1) * part_size_n : i*part_size_n + part_size_n_last;
+		}
+		for(int j = b_start;j<b_end;j++)
+		{
+			for(int k = 0;k<part_size_l;k++)
+			{
+				for(int h = 0;h<m;h++)
+				{
+					matC[j][k] += matA[h][k] * matB[j][h];
+				}
+			}
+		}
+		MPI_Send(&b_start, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
+		MPI_Send(&b_end, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
+		for(int j = b_start;j<b_end;j++)
+		{
+			MPI_Send(&matB[j][0], m, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD);
+		}
+	}
+	for(int i = 0; i<m;i++)
+	{
+		MPI_Gatherv(NULL, 0, MPI_INT, &matC[i][0],&partitions[0], &space[0],  MPI_INT, 0, MPI_COMM_WORLD);
 	}
 }
 
@@ -105,7 +135,8 @@ void worker_task(int rank, int world_size)
 	int l = 0;
 	int n = 0;
 	int part_size_l = 0;
-	int part_size_n = 0;
+	int part_size_n;
+	int part_size_n_last;
 	std::vector<std::vector<double> > matA_part;
 	std::vector<std::vector<double> > matB_part;
 	std::vector<std::vector<double> > matC_part;
@@ -127,12 +158,12 @@ void worker_task(int rank, int world_size)
 	if(n%world_size == 0)
 	{
 		part_size_n = n / world_size;
+		part_size_n_last = part_size_n;
 	}
 	else
 	{
 		part_size_n = n / (world_size-1);
-		if(rank == world_size-1)
-			part_size_n = n % (world_size-1);
+		part_size_n_last = n % (world_size-1);
 	}
 
 
@@ -154,25 +185,45 @@ void worker_task(int rank, int world_size)
 	}
 	matB_part.reserve(part_size_n);
 	std::vector<double> tmp(m);
-	for(int i = 0;i< part_size_n;i++)
+	for(int i = 0;i<part_size_n;i++)
 	{
-		MPI_Status status;
-		MPI_Recv(&tmp[0],m,MPI_DOUBLE,0,0,MPI_COMM_WORLD,&status);
 		matB_part.push_back(tmp);
 	}
+	int b_start;
+	int b_end;
 	for(int i = 0;i< world_size;i++)
 	{
-
-		for(int j = 0;j<m;j++)
+		MPI_Status status;
+		MPI_Recv(&b_start, 1,MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&b_end , 1,MPI_INT, MPI_ANY_SOURCE,0, MPI_COMM_WORLD, &status);
+		for(int i = 0;i< b_end-b_start;i++)
+		{
+			MPI_Recv(&tmp[0],m,MPI_DOUBLE, MPI_ANY_SOURCE,0,MPI_COMM_WORLD,&status);
+			matB_part[i] = tmp;
+		}
+		for(int j = b_start;j<b_end;j++)
 		{
 			for(int k = 0;k<part_size_l;k++)
 			{
-				for(int l = 0;l<m;l++)
+				for(int h = 0;h<m;h++)
 				{
-					matC_part[j][k] += matA_part[l][k] * matB_part[j][l];
+					matC_part[j][k] += matA_part[h][k] * matB_part[j-b_start][h];
 				}
 			}
 		}
+		if(rank!=world_size-1)
+		{
+			MPI_Send(&b_start, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
+			MPI_Send(&b_end, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
+			for(int j = 0;j<b_end-b_start;j++)
+			{
+				MPI_Send(&matB_part[j][0], m, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD);
+			}
+		}
+	}
+	for(int i = 0;i<n;i++)
+	{
+		MPI_Gatherv(&matC_part[i][0],part_size_l, MPI_INT, NULL, NULL, NULL, MPI_INT, 0, MPI_COMM_WORLD);
 	}
 }
 
